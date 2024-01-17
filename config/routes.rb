d09f0960e7e970aace5a2ee5fe8ec1e92fc2a191
MasatoKato <152849350+katopery@users.noreply.github.co>
2024-01-17 15:40:32 +0000

    [Finish] 14~19章

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-
   get 'homes/about', to: 'homes#about', as: 'about'
+  
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  
+  resources :users, only: [:show, :edit, :update]
+
 end
